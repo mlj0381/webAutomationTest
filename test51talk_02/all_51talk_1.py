3866cfe90a654e995f382f2616133ae37d8d94d9
zhangbo666 <78038490@qq.com>
2019-06-26 14:12:39 +0800

    2019-06-26 14:12

--- test51talk_02/all_51talk_1.py
+++ test51talk_02/all_51talk_1.py
@@ -57,10 +57,10 @@ def creatsuite():
     testunit = unittest.TestSuite()
 
     '''启动页弹出层切换'''
-    testunit.addTest(test_startPage.TestStartPage("test_start_page"))
+    # testunit.addTest(test_startPage.TestStartPage("test_start_page"))
 
     '''主站模块TAB切换'''
-    testunit.addTest(test_homePage.TestHomePage("test_homepage_tabpage_switch"))
+    # testunit.addTest(test_homePage.TestHomePage("test_homepage_tabpage_switch"))
 
     '''成人主站TAB切换'''
     testunit.addTest(test_adultHomePage.TestAdultHomePage("test_adulthomepage_tabpage_switch"))
