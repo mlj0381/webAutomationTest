058ddf2e9a9d2c801561eb198a641d1cda20b32c
zhangbo666 <78038490@qq.com>
2019-06-26 12:03:09 +0800

    2019-06-26 12:03

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # webAutomationTest
-web自动化测试
+web自动化测试平台
+
+运行步骤配置信息：
+1、运行之前先修改configuration_files中：driver_configuration.py文件
+   将连接方式修改为：jenkins服务器连接方式
+
+2、用例执行后，需要就发测试报告到相应邮箱里面，不需要可以注释掉发送邮件
+   修改all_51talk_1.py文件：
+
+   generate_testreports：生成测试报告并发送邮件
+   no_testports：不生成测试报告也不发邮件
+
+   如果需要发送测试报告到个人邮箱，需要更改自己到邮箱地址和账户密码即可
+   修改generation_report中：send_Mail.py文件
--- test51talk_02/configuration_files/driver_configurationFiles.py
+++ test51talk_02/configuration_files/driver_configurationFiles.py
@@ -9,7 +9,10 @@ from selenium import webdriver
 
 def browser_driver():
 
-    #服务器：jenkins连接构建时找不到chrome报错时启用
+    """
+        jenkins服务器连接方式
+        服务器：jenkins连接构建时找不到chrome报错时启用
+    """
     option = webdriver.ChromeOptions()
 
     option.binary_location = r"C:\Users\admin\AppData\Local\Google\Chrome\Application\chrome.exe"
@@ -18,12 +21,19 @@ def browser_driver():
     							   executable_path=r"D:\python3\chromedriver.exe")
 
 
+    """
+        本地电脑连接方式：PhantomJS
+    """
     #window--phantomjs
     # obj_driver = webdriver.PhantomJS(executable_path="D:/Python27/phantomjs/bin/phantomjs")
 
     #mac--phantomjs
     # obj_driver = webdriver.PhantomJS(executable_path="/Users/zhangbo/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs")
 
+    """
+        pc与mac普通连接方式
+    """
     # obj_driver = webdriver.Chrome()
+
 
     return obj_driver
